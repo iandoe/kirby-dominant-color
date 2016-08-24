ccba4d7bd4e11cd7ccedc1b3874e3e2982c0d6be
Guillaume Picard <wilhearts@gmail.com>
2016-08-24 17:34:48 +0700

    Initial Commit

---
+++ dominantcolor.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use League\ColorExtractor\Color;
+use League\ColorExtractor\ColorExtractor;
+use League\ColorExtractor\Palette;
+
+kirby()->hook(['panel.file.upload', 'panel.file.replace'], function($image) {
+  if ($image->type() == 'image') {
+		// Create thumb for faster processing
+		$thumb = $image->resize(400);
+
+		// Extract main color
+		$palette = Palette::fromFilename($thumb->root());
+		$extractor = new ColorExtractor($palette);
+		$colors = $extractor->extract(1);
+		$hex_main_color = Color::fromIntToHex($colors[0]);
+
+		// Update image metadata
+		$image->update(array(
+			'color'    => $hex_main_color
+		));
+  }
+});
